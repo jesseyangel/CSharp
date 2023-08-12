2de246b89379465ad9aa4c652b0231acbf647466
Jessey Angel <103680584+jesseyangel@users.noreply.github.com>
2023-08-12 02:59:49 -0300

    Update outras aulas

---
+++ 2-CriandoVariaveis/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {   //Mostrando na tela
+        Console.WriteLine("Projeto 2 - Criando Variáveis");
+        //declarando idade como tipo int
+        int idade;
+        //definindo valor da idade
+        idade = 24;
+        //Concatenando texto com a variavel idade
+        Console.WriteLine("Minha idade e " + idade);
+        //criando uma condicao
+        idade = 24 - 5;
+        //mostrando na tela o resultado da idade agora
+        Console.WriteLine(idade);
+        //Criando nova soma para teste sobre o a ordem dos precedentes 
+        idade = 5 * 2 - 6;
+        //Pedindo para printar a idade 
+        Console.WriteLine(idade);
+
+        Console.WriteLine("Tecle enter para fechar...");
+        Console.ReadLine();
+    }
+}
---
+++ 3 - CriandoVariaveisPontoFlutuante/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {   //Escrevendo na tela
+        Console.WriteLine("Projeto 3 - Variáveis Ponto Flutuante");
+        //Declarando variavel salario como um numero flutuante
+        double salario;
+        //Declarando valor do salario quebrado
+        salario = 3000.10;
+        
+        salario = 3000;
+
+        double idade;
+
+        idade = 7 / 5;
+
+        Console.WriteLine(idade);
+
+        Console.WriteLine(salario);
+        //Aqui so mostrando como se comporta o codigo com tipos de variaveis diferentes
+
+        Console.WriteLine("Tecle enter para fechar...");
+        Console.ReadLine();
+    }
+}
