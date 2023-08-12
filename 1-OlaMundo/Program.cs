04bc9296cd65cfae1d47a7cdb72b8adf0cdfbf71
Jessey Angel <103680584+jesseyangel@users.noreply.github.com>
2023-08-12 02:57:12 -0300

    1 - OlaMundo
    Primeira aula de C# aprendendo conceitos básicos

---
+++ 1-OlaMundo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {   //Esse comando mostra na tela
+        Console.WriteLine("Ola, Mundo!");
+        Console.WriteLine("Tecle enter para fechar ...");
+        //esse comando e um input
+        Console.ReadLine();
+    }
+}
